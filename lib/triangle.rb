6683459726c16e6b645d920e41922fc75559424e
Mohammed Noori <kmanoorib@gmail.com>
2018-07-03 01:09:13 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,13 @@
 class Triangle
   # write code here
-  attr_accessor :side_one, :side_two, :side_three
-  
+  attr_accessor :side_one, :side_two, :side_three, :type
   
   def initialize (attributes)
     attributes.each {|key, value| self.send(("#{key}="), value)}
+  end
+  
+  def find_type
+    
   end
   
   class TriangleError < StandardError
