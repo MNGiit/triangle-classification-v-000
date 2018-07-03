9cae20b519cf5b36d8474b6dff86957868994413
Mohammed Noori <kmanoorib@gmail.com>
2018-07-03 01:14:54 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,10 +1,11 @@
 class Triangle
   # write code here
-  attr_accessor :side_one, :side_two, :side_three, :type
+  attr_accessor :one, :two, :three
   
-  def initialize(attributes)
-    attributes.each {|key, value| self.send(("#{key}="), value)}
-    self.find_type
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
   end
   
   def find_type
